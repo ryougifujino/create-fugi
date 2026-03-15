433f6aa6eddc2a0adb721d8cdb24a5bb1e593506
ryougifujino <ryougifujino@gmail.com>
2026-03-15 23:35:44 +0800

    fix(react-template): @vitejs/plugin-react upgrade error

--- templates/react/vite.config.ts
+++ templates/react/vite.config.ts
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-    tailwindcss(),
-  ],
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] }), tailwindcss()],
 })
